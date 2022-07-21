3a8cd77c0fb0405eb2bdeb998aaa942423cd2207
Flavia de Almeida <flaviatait@gmail.com>
2022-07-20 22:01:50 -0300

    Exercicio feito com a professora

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,108 @@
 //CODIGO AQUI
+/*let saldo = 15.00
+saldo = saldo + 22.40
+console.log(saldo)
+
+saldo-=12.34
+console.log(saldo)
+
+saldo = saldo/3
+console.log(saldo);
+
+saldo = saldo*3.42
+console.log(saldo);
+
+saldo = saldo%3
+console.log(saldo);
+
+//replica
+
+//Tenho 75,20 e ganho 253.00. Gastei 22.00. Dividi o que sobrou entre a Lu, a Leck e eu. Apliquei e rendeu 4.5.
+let conta = 75.20
+conta = conta + 253.00
+console.log(conta);
+
+conta-=22
+console.log(conta)
+
+conta = conta/3
+console.log(conta);
+
+conta = conta*4.5
+console.log(conta);
+
+conta = conta%3
+console.log(conta);
+
+const somar = 2*2+2;
+console.log(somar);
+
+const somar1 = 2*(2+2)
+console.log(somar1);
+
+//replica
+
+const somar2 = 45*32+50
+console.log(somar2);
+
+const somar3 = 45*(32+50)
+console.log(somar3);*/
+
+
+const result1 = 5===5;
+console.log(result1);
+
+const result2 = 5!==5;
+console.log(result2)
+
+const result3 = 5!==5;
+console.log(result3)
+
+const result4 = '5'==='cinco'
+console.log(result4)
+
+const result5 = typeof 5 === typeof 20
+console.log(result5)
+
+const result6 = typeof '5' === typeof 'cinco'
+console.log(result6)
+
+const num1=5
+const num2=2
+
+console.log('O primeiro número é igual ao segundo número?', num1===num2);
+
+console.log('O primeiro número é menor ou igual ao segundo número?', num1<=num2);
+
+console.log('O primeiro número é maior que o segundo número?', num1>num2);
+
+console.log('O primeiro número é menor que o segundo número?', num1<num2);
+
+const a= "Quero dirigir um celta 2012";
+const b= "Sou  maior de idade";
+const c= 'Consigo comprar o Celta por 22.000 reais';
+const d= 'Consigo alugar o Celta';
+const e= 'Tenho carteira de motorista';
+
+//quero dirigir um celta 2012
+//não sou maior de idade
+//sou maior de idade e não tenho carteira de motorista
+//sou maior de idade e consigo comprar ou alugar um celta
+// sou maior de idade e tenho carteira de motorista, mas não quero dirigir um celta
+
+/*exercicio de fixação:
+faça um programa que:
+pergunta a sua idade;
+pergunta a idade do seu melhor amigo(a);
+Imprima a seguinte mensagem "sua idade é maior do que a do seu"
+melhor amigo(a)?;
+Imprima no console a diferença de idade*/
+
+let idade = prompt("Qual é a sua idade?");
+let idadeAmigo = prompt("Qual a idade do seu melhor amigo?");
+console.log("Sua idade é maior que a do seu/sua melhor amigo(a)?", idade>idadeAmigo)
+console.log(idade - idadeAmigo)
+
+//Exercício 2
+a = "vou para a praia"
+b = "sou maior de idade"
